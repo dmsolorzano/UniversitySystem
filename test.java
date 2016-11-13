979a1556cce2a81400cf9b7420a219fc90ac0a84
dmsolorzano <dmsolorzano@miners.utep.edu>
2016-11-12 20:41:22 -0700

    Approved
    GUI changes

--- test.java
+++ test.java
@@ -1,18 +1,19 @@
+import java.sql.*;
 import java.io.IOException;
 /** The test class is used to run simulations to ensure the system is working as intended
  * @author Darren Solorzano
  * @author Reynaldo Martinez
  * @author Chris Santos
- * @version 1.3
+ * @version 1.4
  * */
 public class test {
-	public static void main(String[] args) throws IOException{
+	/*public static void main(String[] args) throws IOException{
 		Admin a = new Admin(0, null, null, null); //Administrator object
 		Course c = new Course("CS 3331", "arbitarydate", 101); // Course object
 		//a.createCourse(c.getCourseName(), c.getcRN());
 		//a.removeCourse("jpo", 102);
 		a.createAccount();
 		
-	}
+	}*/
 	
 }
