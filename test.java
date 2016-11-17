17688da8400eaebe87aefecc2c61f70f192f05c7
dmsolorzano <dmsolorzano@miners.utep.edu>
2016-11-16 21:17:38 -0700

    fixed errors in the createDatabase method

--- test.java
+++ test.java
@@ -6,26 +6,16 @@ import java.io.IOException;
  * @author Chris Santos
  * @version 1.4
  * */
-public class test extends User{
-	
-	public test(int id, String name, String user, String pass) {
-		super(id, name, user, pass);
-		// TODO Auto-generated constructor stub
-	}
-
-	
-	
+public class test{
 	public static void main(String[] args) throws Exception{
-		User u = new User(90, "name", "name", "name");
+		User u = new User(90, "name", "name", "name"); //User object
 		Admin a = new Admin(0, null, null, null); //Administrator object
 		Course c = new Course("CS 3331", "arbitarydate", 101); // Course object
-		// We can drop the university table so we don't run into errors
-		u.createDatabase();
-
-		//a.createCourse(c.getCourseName(), c.getcRN());
-		//a.removeCourse("jpo", 102);
-		//a.createAccount();
+		Object[] courses = null;
 		
+		a.createDatabase();
+		//courses[0]=a.createCourse();
+
 	}
 	
 }
